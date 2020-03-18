a920bfc9f302ed59d3dc9e0d2e8af5d7e684ed26
Clemens Kaserer <clemens.kaserer@gmail.com>
2020-03-18 13:38:01 +0100

    added vim package

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -9,3 +9,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_net_tools(host):
     net_tools = host.package("net-tools")
     assert net_tools.is_installed
+
+
+def test_wget(host):
+    wget = host.package("wget")
+    assert wget.is_installed
+
+
+def test_vim(host):
+    wget = host.package("vim")
+    assert wget.is_installed
